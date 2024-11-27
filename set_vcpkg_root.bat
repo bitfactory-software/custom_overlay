a8da3c3d61749884efb3ccb6b341ef85a1b8b507
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-11-27 08:32:18 +0100

    fix bat

--- set_vcpkg_root.bat
+++ set_vcpkg_root.bat
@@ -5,10 +5,11 @@ to custom_overlay
 vcpkg remove virtual_void
 vcpkg install virtual-void --overlay-ports=D:\BitFactory\custom_overlay
 
-tp vcpkg.git
+to vcpkg.git
 Copy-Item -Path D:\BitFactory\custom_overlay\virtual-void -Destination D:\BitFactory\vcpkg\ports\virtual-void 
+vcpkg format-manifest "D:\BitFactory\vcpkg\ports\virtual-void\vcpkg.json"
 git add ports/virtual-void
 git commit -m "Add virtual-void to vcpkg"
-vcpkg format-manifest "D:\BitFactory\vcpkg\ports\virtual-void\vcpkg.json"
 vcpkg x-add-version virtual-void
 git add versions/v-/virtual_void.json
+git commit -m "Add virtual-void version to vcpkg"
